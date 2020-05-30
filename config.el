c4fe246b6a7ef7d87d591aa313c8b23c47ce1c9e
Danilo Alves <danilovieiraalves@hotmail.com>
2020-05-30 16:59:37 -0300

    feat(config): Adds global key binding ":" to evil-command-window-ex

--- config.el
+++ config.el
@@ -3,6 +3,8 @@
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
 
+;; Global binding
+(evil-define-key 'normal global-map (kbd ":") 'evil-command-window-ex)
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets.
